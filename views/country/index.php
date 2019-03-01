e63567fa26a526a75920314e08fe08836719c48a
蒋建明 <jiangjianming@bmkp.cn>
2019-03-01 19:05:00 +0800

    更新controller

---
+++ views/country/index.php
@@ -0,0 +1,13 @@
+<?php
+use yii\helpers\Html;
+use yii\widgets\LinkPager;
+?>
+<ul>
+    <?php foreach($countries as $country): ?>
+    <li>
+        <?= Html::encode("{$country->name} ({$country->code})")?>
+        <?= $country->population?>
+    </li>
+    <?php endforeach; ?>
+</ul>
+<?= LinkPager::widget(['pagination' => $pagination]) ?>
